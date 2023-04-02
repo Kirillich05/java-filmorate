aa737b7e9afee541086a8a4e84397dbc00a7b98e
Kirillich05 <khryapin05@yandex.ru>
2023-04-02 18:46:32 +0300

    feat: добавить сервис для пользователей по добавлению, удалению, вывод друзей и сервис для фильмов по добавлению, удалению лайков и вывода топ фильмов по лайкам; refactor: создать интерфейсы FilmStorage, UserStorage; style: добавить пробелы вокруг = в классе User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -19,7 +19,7 @@ public class User {
     @NotNull
     private String login;
     private String name;
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @NotNull
     private LocalDate birthday;
 
